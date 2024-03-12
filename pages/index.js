5f574b468d287def01656f0263c5a2d302e8f0bd
Vladyslav <115696730+vla-decr000@users.noreply.github.com>
2024-03-12 10:57:39 +0200

    Update index.js - remain only hard projects

--- pages/index.js
+++ pages/index.js
@@ -179,33 +179,7 @@ export default function Home() {
                                 <Image alt={'img'} src={web4} className={'rounded-t-lg object-cover '} width={'100%'} height={'100%'}   /></div>
                         </div>
  
-                        <div className={'web__item  basis-1/3 shadow-lg flex-1 relative'} onMouseOver={() => setHoverState3(true)} onMouseOut={() => setHoverState3(false)}>
-                                {hoverState3 ?  
-                                <a className={'item__link  inset-0 absolute  z-100 text-white flex' +
-                                    ' justify-center' +
-                                    ' items-center'} href="https://wizard-avtocolor.com" target="_blank">
-                                <span className={' bg-gradient-to-r from-cyan-500 to-teal-500 '}>{langState ? 'Переглянути':'Watch' +
-                                    ' this'}</span>
-                                </a> : ''}
-                                <div><Image alt={'img'} src={web3} className={'min-h-20 rounded-t-lg object-cover '} width={'100%'} height={'100%'}   /></div>
-                            </div>
-                            <div className={'web__item basis-1/3 shadow-lg  flex-1  relative'} onMouseOver={() => setHoverState5(true)} onMouseOut={() => setHoverState5(false)}>
-                            {hoverState5 ?   <a className={'item__link  inset-0 absolute  z-100 text-white flex' +
-                                ' justify-center' +
-                                ' items-center'} href={'https://deliever.vercel.app/'} target={'_blank'} rel={'noreferrer'} >
-                                <span className={' bg-gradient-to-r from-cyan-500 to-teal-500 '}>{langState ? 'Переглянути':'Watch this'}</span>
-                            </a> : ''}
-                            <div>
-                                <Image alt={'img'} src={web5} className={'rounded-t-lg object-cover'} width={'100%'} height={'100%'}   /></div>
-                        </div>
-                        
-                        <div className={'web__item  basis-1/3 shadow-lg flex-1 relative' } onMouseOver={() => setHoverState(true)} onMouseOut={() => setHoverState(false)}>
-                             {hoverState ?   <a className={'item__link  inset-0 absolute  z-100 text-white flex justify-center' +
-                                ' items-center'} href={'https://vla-decr000.github.io/Lux/'} target={'_blank'} rel={'noreferrer'}>
-                                <span className={' bg-gradient-to-r from-cyan-500 to-teal-500 '}>{langState ? 'Переглянути':'Watch this'}</span>
-                            </a> : ''}
-                            <div><Image alt={'img'} src={web1} className={' rounded-t-lg object-cover '} width={'100%'} height={'100%'}   /></div>
-                        </div>
+                   
                        
                        
                       
