85ae96e445fa3eafd273c7b0ea1aa1dd14b6e5c6
Vla_decr <vlaadecr000@gmail.com>
2024-11-27 23:28:55 +0200

    Bitnews

--- pages/index.js
+++ pages/index.js
@@ -256,19 +256,6 @@ export default function Home() {
                             </a> : ''}
                             <div><Image alt={'img'} src={hairtalk} className={' rounded-t-lg object-cover '} width={'100%'} height={'100%'}  /> </div>
                         </div>
-                        
-             
-                        
-                                
-
-                                                                                      
-                
-                 
- 
-                   
-                       
-                       
-                      
                       
                                             </div>
 
